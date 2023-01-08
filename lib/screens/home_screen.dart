e4db45a6e689f14893af622fa6118bf0218038a3
Roy Kang (Sungtae, Kang) <roykang75@gmail.com>
2023-01-09 00:31:57 +0900

    Update home_screen.dart
    code challenge

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -47,6 +47,12 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void onResetPressed() {
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+    });
+  }
+
   String format(int seconds) {
     var duration = Duration(seconds: seconds);
 
@@ -75,15 +81,28 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           Flexible(
             flex: 2,
-            child: Center(
-              child: IconButton(
-                iconSize: 120,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunnung ? onPausePressed : onStartPressed,
-                icon: Icon(isRunnung
-                    ? Icons.pause_circle_outline
-                    : Icons.play_circle_outline),
-              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: IconButton(
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                    onPressed: isRunnung ? onPausePressed : onStartPressed,
+                    icon: Icon(isRunnung
+                        ? Icons.pause_circle_outline
+                        : Icons.play_circle_outline),
+                  ),
+                ),
+                Center(
+                  child: IconButton(
+                    iconSize: 120,
+                    color: Theme.of(context).cardColor,
+                    onPressed: onResetPressed,
+                    icon: const Icon(Icons.restore_outlined),
+                  ),
+                ),
+              ],
             ),
           ),
           Flexible(
